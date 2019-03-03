548d520d5bf564265f769098d620f6b37da81886
Eric winnington <eric.in.the.net@gmail.com>
2019-03-03 14:00:58 +0100

    Add project files.

---
+++ wfRunnerCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wfRunnerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
